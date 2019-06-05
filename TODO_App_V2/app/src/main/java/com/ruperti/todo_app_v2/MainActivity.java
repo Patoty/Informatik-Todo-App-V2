c144ab6d9aa5149d93e1a6bd469f463a28123b98
RichiJa <49656073+RichiJa@users.noreply.github.com>
2019-06-05 11:11:27 +0200

    X

--- TODO_App_V2/app/src/main/java/com/ruperti/todo_app_v2/MainActivity.java
+++ TODO_App_V2/app/src/main/java/com/ruperti/todo_app_v2/MainActivity.java
@@ -1,14 +1,29 @@
 package com.ruperti.todo_app_v2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton PlusButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        PlusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityCreateTask();
+            }
+        });
+
+    }
+    private void openActivityCreateTask() {
+        Intent intent = new Intent(this,CreateTaskActivity.class);
+        startActivity(intent);
     }
 }
