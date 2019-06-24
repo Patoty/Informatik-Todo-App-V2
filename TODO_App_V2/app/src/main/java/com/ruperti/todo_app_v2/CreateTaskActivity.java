4dbaf1b1397a5b0bded721011720c2017081ace4
RichiJa <49656073+RichiJa@users.noreply.github.com>
2019-06-24 10:25:44 +0200

    x

--- TODO_App_V2/app/src/main/java/com/ruperti/todo_app_v2/CreateTaskActivity.java
+++ TODO_App_V2/app/src/main/java/com/ruperti/todo_app_v2/CreateTaskActivity.java
@@ -2,14 +2,32 @@ package com.ruperti.todo_app_v2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 public class CreateTaskActivity extends AppCompatActivity {
+
+    String Task_Name;
+    int Task_Date;
+    int Task_Time;
+
+    EditText Task_NameInput;
+    EditText Task_DateInput;
+    EditText Task_TimeInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
 
+        Task_NameInput = (EditText)findViewById(R.id.Text_Task_Name);
+        Task_DateInput = (EditText)findViewById(R.id.Text_Task_Date);
+        Task_TimeInput = (EditText)findViewById(R.id.Text_Task_Time);
+
+        Task_NameInput.setOnClickListener(new OnClickListener());
+        Task_DateInput.setOnClickListener(new OnClickListener());
+        Task_TimeInput.setOnClickListener(new OnClickListener());
     }
 
 }
